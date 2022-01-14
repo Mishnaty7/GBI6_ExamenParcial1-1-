2aa749a811e3aa48b0503168cd2b10a26305be8d
Mishelle Culcay <mishelle.culcay@est.ikiam.edu.ec>
2022-01-13 20:00:00 -0500

    Culcay Mishelle ha resuelto el ejercicio 5

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -9,5 +9,16 @@ cut -f 1 Gesquiere2011_data.csv | grep -c -w 27
 
 #Write a script taking as input the file name and the ID of the individual,and returning the number of records for that ID.
 
+cut -f 1 Gesquiere2011_data.csv | grep -w -c 27
 
 #Write a script that returns the number of times each individual was sampled.
+
+tail -n +2 Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq
+maleID=$(tail -n +2 Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq)
+echo $maleID
+for a in $maleID
+do
+muestra=$(cut -f 1 Gesquiere2011_data.csv | grep -w -c $a)
+echo "maleID:" $a "muestras:" $muestra
+done
+
