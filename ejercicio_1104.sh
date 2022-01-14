e3750acf1abed8ee289d93d115b6cde978b9f367
Mishelle Culcay <mishelle.culcay@est.ikiam.edu.ec>
2022-01-14 12:15:33 -0500

    Culcay Mishelle ha corregido ejercicio 4

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -3,13 +3,13 @@
 #1.10.4 Data Explorer
 
 #Column name:
-cut -d ',' -f 7 Buzzard2015_data.csv | head -n 1
+cut -d ',' -f 7 data/Buzzard2015_data.csv | head -n 1
 
 #Number of distinct values
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort | uniq | wc -l
+cut -d ',' -f 7 data/Buzzard2015_data.csv | tail -n +2 | sort | uniq | wc -l
 
 #Minimum value
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort -n | head -n 1
+cut -d ',' -f 7 data/Buzzard2015_data.csv | tail -n +2 | sort -n | head -n 1
 
 #Maximum value
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort -n | tail -n 1
+cut -d ',' -f 7 data/Buzzard2015_data.csv | tail -n +2 | sort -n | tail -n 1
