58e9da8792f42d866ffa0b6f31cc1488ab4d5b26
Mishelle Culcay <mishelle.culcay@est.ikiam.edu.ec>
2022-01-14 12:23:06 -0500

    Culcay Mishelle ha resuelto ejercicio 5

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -4,21 +4,21 @@
 
 #How many times were the levels of individuals 3 and 27 recorded?
 
-cut -f 1 Gesquiere2011_data.csv | grep -c -w 3
-cut -f 1 Gesquiere2011_data.csv | grep -c -w 27
+cut -f 1 data/Gesquiere2011_data.csv | grep -c -w 3
+cut -f 1 data/Gesquiere2011_data.csv | grep -c -w 27
 
 #Write a script taking as input the file name and the ID of the individual,and returning the number of records for that ID.
 
-cut -f 1 Gesquiere2011_data.csv | grep -w -c 27
+cut -f 1 data/Gesquiere2011_data.csv | grep -w -c 27
 
 #Write a script that returns the number of times each individual was sampled.
 
-tail -n +2 Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq
-maleID=$(tail -n +2 Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq)
+tail -n +2 data/Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq
+
+maleID=$(tail -n +2 data/Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq)
 echo $maleID
 for a in $maleID
 do
-muestra=$(cut -f 1 Gesquiere2011_data.csv | grep -w -c $a)
+muestra=$(cut -f 1 data/Gesquiere2011_data.csv | grep -w -c $a)
 echo "maleID:" $a "muestras:" $muestra
 done
-
